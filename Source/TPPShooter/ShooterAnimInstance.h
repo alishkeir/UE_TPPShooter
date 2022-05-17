6af30e6813d0e1bec501e16017cb5c8bf2cf8289
alishkeir <alishkeir_@hotmail.com>
2022-05-17 21:20:09 +0300

    Created AnimInstance

---
+++ Source/TPPShooter/ShooterAnimInstance.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "ShooterAnimInstance.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class TPPSHOOTER_API UShooterAnimInstance: public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override; /// same as BeginPlay() for  Actor
+
+	UFUNCTION(BlueprintCallable)
+		void UpdateAnimationProperties(float DeltaTime);  /// same as Tick() for Actor
+
+private:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		class AShooterCharacter* ShooterCharacter;
+
+	/// Character speed
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		float Speed;
+
+	/// If character is in the air
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		bool bIsInAir;
+
+	/// If character is moving
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement", meta = (AllowPrivateAccess = "true"))
+		bool bIsAccelerating;
+};
